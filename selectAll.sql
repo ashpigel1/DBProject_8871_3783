a839ebf8f84c6f01ef2d2a569f999c1bb9f50701
Shpigel <almog.shpigel@intel.com>
2024-05-21 12:33:40 +0300

    Added selectAll file

---
+++ selectAll.sql
@@ -0,0 +1,28 @@
+-- selectAll.sql
+
+-- Select all records from the Rifle_Type table
+SELECT * FROM Rifle_Type;
+
+-- Select all records from the Rifle table
+SELECT * FROM Rifle;
+
+-- Select all records from the Soldier table
+SELECT * FROM Soldier;
+
+-- Select all records from the Armory_worker table
+SELECT * FROM Armory_worker;
+
+-- Select all records from the Attachment table
+SELECT * FROM Attachment;
+
+-- Select all records from the Ammo table
+SELECT * FROM Ammo;
+
+-- Select all records from the Testing table
+SELECT * FROM Testing;
+
+-- Select all records from the Soldier_Special_qualification table
+SELECT * FROM Soldier_Special_qualification;
+
+-- Select all records from the Order_ammo table
+SELECT * FROM Order_ammo;
